890c5bf124675262c6f26cb64d53f67b0d7e5933
Simon Moyana <therealskm@hotmail.com>
2023-02-19 14:19:32 +0000

    Player Controller Update

--- DVSimonsTest/Assets/Scripts/PlayerMovement.cs
+++ DVSimonsTest/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [HideInInspector] public GameObject player;
+    [HideInInspector] public Rigidbody rb;
     public Transform cam;
     public int speed;
+    public int maxVelocity;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         player = this.gameObject;
+        rb = this.gameObject.GetComponent<Rigidbody>();
         animator = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-        CameraFollowPosition();
+        PlayerRotation();
+        //Movement();
+        CharacterController();
     }
 
     public void Movement()
@@ -33,6 +37,18 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("RotationMagnitude", player.transform.rotation.normalized.y);*/
     }
 
+    public void CharacterController()
+    {
+        Vector3 direction = player.transform.forward * Input.GetAxis("Vertical") + player.transform.right * Input.GetAxis("Horizontal");
+        if (rb.velocity.magnitude <= maxVelocity)
+        {
+            rb.AddForce(direction.normalized * speed, ForceMode.Force);
+        }
+        animator.SetFloat("SpeedX", Input.GetAxis("Horizontal"));
+        animator.SetFloat("SpeedZ", Input.GetAxis("Vertical"));
+        animator.SetFloat("H_Velocity", rb.velocity.magnitude);
+    }
+
     public void CameraFollowPosition()
     {
         Vector3 position = new Vector3(player.transform.position.x, player.transform.position.y + 3, player.transform.position.z - 5);
@@ -40,4 +56,13 @@ public class PlayerMovement : MonoBehaviour
         //Vector3.Lerp(cam.position, position, 0.5f);
 
     }
+
+    public void PlayerRotation()
+    {
+        player.transform.rotation = Quaternion.Euler(new Vector3(player.transform.rotation.x, cam.rotation.normalized.y * 180, player.transform.rotation.z));
+        //Vector3 lookDirection = player.transform.forward * Input.GetAxis("Vertical") + player.transform.right * Input.GetAxis("Horizontal");
+        //player.transform.forward = Vector3.Slerp(player.transform.forward, lookDirection, Time.deltaTime * 7);
+    }
+
+
 }
